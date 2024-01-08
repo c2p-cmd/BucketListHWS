ab46b2401ab3b66a16026f01fc08fd822b9c5fe4
Sharan Thakur <sharandt19@live.com>
2024-01-08 12:38:23 +0530

    Initial Commit

---
+++ BucketListHWS/BucketListHWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListHWSApp.swift
+//  BucketListHWS
+//
+//  Created by Sharan Thakur on 08/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListHWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketListHWS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BucketListHWS
+//
+//  Created by Sharan Thakur on 08/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
