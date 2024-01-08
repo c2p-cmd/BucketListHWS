49241ca579da0702478f0adc038a10d38c0d915d
Sharan Thakur <sharandt19@live.com>
2024-01-08 15:16:41 +0530

    Edit

--- BucketListHWS/Models/Extension.swift
+++ BucketListHWS/Models/Extension.swift
@@ -5,4 +5,44 @@
 //  Created by Sharan Thakur on 08/01/24.
 //
 
-import Foundation
+import MapKit
+import SwiftUI
+
+extension Location {
+    static let buckinghamPalace = Location(
+        "Buckingham Palace",
+        description: "The Buckingham Palace",
+        longitude: -0.141,
+        latitude: 51.501
+    )
+    
+    static let towerOfLondon = Location(
+        "Tower Of London",
+        description: "The Tower of London",
+        longitude: -0.076,
+        latitude: 51.508
+    )
+}
+
+extension MapCameraPosition {
+    static let london = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
+    
+    static let paris = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
+    
+    static let tokyo = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
+}
--- BucketListHWS/Models/Models.swift
+++ BucketListHWS/Models/Models.swift
@@ -5,4 +5,34 @@
 //  Created by Sharan Thakur on 08/01/24.
 //
 
-import Foundation
+import MapKit
+
+fileprivate func urlString(latitude: String, longitude: String) -> String {
+    "https://en.wikipedia.org/w/api.php?ggscoord=\(latitude)%7C\(longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+}
+
+struct Location: Identifiable, Codable, Equatable {
+    var id: UUID
+    var name: String
+    var description: String
+    var longitude: Double
+    var latitude: Double
+    
+    init(_ name: String, description: String, longitude: Double, latitude: Double) {
+        self.id = UUID()
+        self.name = name
+        self.description = description
+        self.longitude = longitude
+        self.latitude = latitude
+    }
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    static func==(lhs: Location, rhs: Location) -> Bool {
+        lhs.longitude == rhs.longitude && lhs.latitude == rhs.longitude
+    }
+}
+
+typealias Locations = [Location]
