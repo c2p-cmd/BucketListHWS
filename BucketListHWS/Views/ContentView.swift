bb200a13d949af36a72879b92a4181e8feea6868
Sharan Thakur <sharandt19@live.com>
2024-01-08 17:09:41 +0530

    Added biometrics to app

--- BucketListHWS/Views/ContentView.swift
+++ BucketListHWS/Views/ContentView.swift
@@ -5,20 +5,66 @@
 //  Created by Sharan Thakur on 08/01/24.
 //
 
+import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.scenePhase) var scenePhase
+    
+    @State private var isUnlocked = false
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
+            if isUnlocked {
+                MainView()
+            } else {
+                Image(systemName: "lock.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 50)
+                    .padding(.bottom)
+                Text("Unlock your app before using all its features!")
+                    .bold()
                 
-                NavigationLink("MainView", destination: MainView.init)
+                Spacer()
+                
+                Button("Unlock with Biometrics", action: authenticate)
+                .tint(.pink)
+                .buttonStyle(.borderedProminent)
+                .padding(.bottom, 50)
             }
-            .padding()
+        }
+        .onChange(of: scenePhase) {
+            switch scenePhase {
+            case .background, .inactive:
+                isUnlocked = false
+            case .active:
+                break
+            @unknown default:
+                fatalError()
+            }
+        }
+        .preferredColorScheme(.dark)
+    }
+    
+    private func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Please authenticate yourself to unlock your places"
+            
+            Task {
+                do {
+                    let success = try await context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason)
+                    
+                    isUnlocked = success
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        } else {
+            print("No biometrics!")
         }
     }
 }
